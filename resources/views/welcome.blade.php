c36c94fb9e2ab4d2944b672bfc4f955e7e81338d
vietnt <vietnt@vnext.com.vn>
2019-05-18 14:12:29 +0700

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>Divisima | eCommerce Template</title>
+        <link href="{{ asset('img/favicon.ico') }}" rel="shortcut icon"/>
 
         <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     </head>
